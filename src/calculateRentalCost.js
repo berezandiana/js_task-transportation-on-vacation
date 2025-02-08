b52043ddd710eed8f56d44cc2500cebc37dd55e1
Diana <berezandiana24@gmail.com>
2025-02-08 22:13:20 +0100

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const baseCost = 40;
+  const basicDiscount = 20;
+  const additionalDiscount = 50;
+  const basicDiscountDays = 3;
+  const additionalDiscountDays = 7;
+
+  const fullPrice = baseCost * days;
+
+  if (days >= additionalDiscountDays) {
+    return fullPrice - additionalDiscount;
+  }
+
+  if (days >= basicDiscountDays) {
+    return fullPrice - basicDiscount;
+  }
+
+  return fullPrice;
 }
 
 module.exports = calculateRentalCost;
